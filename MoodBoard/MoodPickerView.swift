0eb45d42f4632f134e6e5613e20fa940f721cab5
AnnaHoerschgen <ah0959247@otc.edu>
2025-04-16 09:11:23 -0500

    Mood Picker finished

---
+++ MoodBoard/MoodPickerView.swift
@@ -0,0 +1,40 @@
+//
+//  MoodPickerView.swift
+//  MoodBoard
+//
+//  Created by HOERSCHGEN, ANNA M. on 4/16/25.
+//
+
+import SwiftUI
+
+struct MoodPickerView: View {
+    @Binding var mood: Mood
+    @Environment(\.dismiss) var dismiss
+    var body: some View {
+        List {
+            ForEach(Mood.allCases) { moodOption in
+                Button(action: {
+                    mood = moodOption
+                    dismiss()
+                }) {
+                    HStack {
+                        Text(moodOption.rawValue.capitalized)
+                            .foregroundColor(.primary)
+                        Spacer()
+                        if mood == moodOption {
+                            Image(systemName: "checkmark")
+                                .foregroundColor(.blue)
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+#Preview {
+    MoodPickerView()
+}
+
+
+// Note: Pass binding with MoodPickerView(mood: $selectedMood)
