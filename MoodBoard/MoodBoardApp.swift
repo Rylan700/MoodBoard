4e2c0f98ad8556aa44ce7e5ac7ce96a64cd8ddb1
Rylan700 <rw0971064@otc.edu>
2025-04-16 08:29:29 -0500

    Initial Commit

---
+++ MoodBoard/MoodBoardApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoodBoardApp.swift
+//  MoodBoard
+//
+//  Created by WADE, RYLAN on 4/16/25.
+//
+
+import SwiftUI
+
+@main
+struct MoodBoardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
